e798a167506b4afc32c533c1323a4f3ffa6974a3
SubhamPRO <prosubhamdas@yahoo.com>
2021-03-12 18:12:10 +0530

    Update index.vue
    Changes on dashboard

--- pages/app/index.vue
+++ pages/app/index.vue
@@ -26,14 +26,14 @@
     <v-flex xs12 sm12 md6>
       <v-card class="ma-2 red">
         <v-responsive aspect-ratio="4" class="d-flex justify-center align-center">
-          <div class="display-1 white--text text-center">Your Username : {{ user.username }} </div>
+          <div class="display-1 white--text text-center">Username : {{ user.username }} </div>
         </v-responsive>
       </v-card>
     </v-flex>
     <v-flex xs12 sm12 md6>
       <v-card class="ma-2 teal">
         <v-responsive aspect-ratio="4" class="d-flex justify-center align-center">
-          <div class="display-1 white--text text-center">Wallet Balance : {{ Number(user.balance).toFixed(2) }} INR </div>
+          <div class="display-1 white--text text-center">WBalance : {{ Number(user.balance).toFixed(2) }} INR </div>
         </v-responsive>
       </v-card>
     </v-flex>
